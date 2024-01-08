5a321cad59fac62c8a1b8c20b7bda915b4aa1a06
Nicholas <aigberuan6@gmail.com>
2024-01-08 14:31:25 -0800

    Py

---
+++ word_replacement.py
@@ -0,0 +1,10 @@
+def replace_word():
+    
+    str = input('type the string used in the replacement')
+    word_to_replace = input("enter the word to replace: ")
+    word_replacemet = input('enter the word replacement: ')
+    print(str.replace(word_to_replace,word_replacemet))
+    
+    
+    
+replace_word()    
