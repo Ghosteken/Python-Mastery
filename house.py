c27d9327ace8aef3276cb2e72773e316f2769270
nicholas <aigberuan6@gmail.com>
2024-08-06 10:50:38 +0100

    loops

---
+++ house.py
@@ -0,0 +1,28 @@
+
+
+# # name = input("what is your name")
+# # if name == 'harry':
+# #     print("hermonie")
+# # elif name == 'draco':
+# #     print("sly")
+
+# # #match keyword
+# # match name:
+# #     case 'harryo' | 'bgood':
+# #         print("good")
+# #     case 'food':
+# #         print("tinumbu") 
+# #     case _:
+# #         print("who??")         
+        
+# #loop
+# i = 0
+# while i<=2:
+#     print("meow")
+#     i +=1  
+           
+print("good\n"*3,end="")
+
+
+for _ in range(3):
+    print("meow")            
