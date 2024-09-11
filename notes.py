297672f42916a3e572b2ada9b7093f593db5276d
nicholas <aigberuan6@gmail.com>
2024-09-11 15:59:27 +0100

    commit

--- notes.py
+++ notes.py
@@ -1,59 +1,60 @@
-# # # # # name = input(("what is your name ? ")).strip().title()
+# # # # # # name = input(("what is your name ? ")).strip().title()
 
 
-# # # # # #remove the witespace from str and captitalize
-# # # # # name = name.strip().title()
+# # # # # # #remove the witespace from str and captitalize
+# # # # # # name = name.strip().title()
 
-# # # # # first,last = name.split(" ")
+# # # # # # first,last = name.split(" ")
 
-# # # # # print(f'my name is {name}' )
-
-
-
-# # # # def hello(to="world"):
-# # # #    print("hello", to)
-
-
-# # # # hello()    
-# # # # name =  input(' what is your name?? ')
-# # # # hello(name)        
+# # # # # # print(f'my name is {name}' )
 
 
 
-# # # def main():
-# # #     x = int(input("what is x? "))
-# # #     print("x is x2", square(x))
+# # # # # def hello(to="world"):
+# # # # #    print("hello", to)
+
+
+# # # # # hello()    
+# # # # # name =  input(' what is your name?? ')
+# # # # # hello(name)        
+
+
+
+# # # # def main():
+# # # #     x = int(input("what is x? "))
+# # # #     print("x is x2", square(x))
     
-# # # def square(n):
-# # #     return n * n or pow(n,2)   
+# # # # def square(n):
+# # # #     return n * n or pow(n,2)   
 
-# # # main()
+# # # # main()
 
-# # '''conditonals'''
-# # x = int(input("what is x"))
-# # y = int(input("what is y"))
+# # # '''conditonals'''
+# # # x = int(input("what is x"))
+# # # y = int(input("what is y"))
 
-# # if x > y:
-# #     print("x is less than y")
-# # else:
-# #     print("no love")            
+# # # if x > y:
+# # #     print("x is less than y")
+# # # else:
+# # #     print("no love")            
 
-# # score = int(input("score: "))
-# # if score >= 90 and score <= 100:
-# #     print("Grade A")
+# # # score = int(input("score: "))
+# # # if score >= 90 and score <= 100:
+# # #     print("Grade A")
 
-# def cube(n): 
-#     print(n*3)
+# # def cube(n): 
+# #     print(n*3)
     
-# num = int(input(print("what is your number")))
-# print(cube(num))
+# # num = int(input(print("what is your number")))
+# # print(cube(num))
 
-x = int(input("what is x"))
+# x = int(input("what is x"))
 
-if x % 2 == 0:
-    print("even")
-else:
-    print("odd")      
+# if x % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")      
+
 
         
      
