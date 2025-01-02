9973fb8a70d41d79d6ba7a030925eec56ae094da
nicholas <aigberuan6@gmail.com>
2025-01-02 21:36:11 +0100

    2

--- test1.py
+++ test1.py
@@ -1,6 +1,33 @@
+# # # # from random import choice
+# # # # coin = choice(["heads","tails"])
+# # # # print(coin)
 
-from random import choice
+# # # import random
 
-coin = choice(["heads","tails"])
+# # # number = random.randint(1,10)
 
-print(coin) 
+# # import random
+
+# # cards = ["jacj","boy","do"]
+
+# # random.shuffle(cards)
+# # for card in cards:
+# #     print(card)
+# import statistics
+
+# avg = statistics.mean([100,90])
+# print(avg)
+# '''command line arg'''
+# import sys
+# try:
+#     print('hello  my name is', sys.argv[1])
+# except IndexError:
+#     print('too few args') 
+import sys
+
+if len(sys.argv) < 2:
+    sys.exit("too few args")
+elif len(sys.argv) > 2:
+    sys.exit("too many args")
+
+print('hello my name is', sys.argv[1])           
